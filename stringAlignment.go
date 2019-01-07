2811804e703adb5cb5da71037984d0f227586779
luca <luca@deskluc.fritz.box>
2019-01-07 22:06:43 +0100

    adding first functunality

--- stringAlignment.go
+++ stringAlignment.go
@@ -1,1 +1,9 @@
-package aeStringAlignment
+package main
+
+func main() {
+	aeae := "/home/luca/Dosuments/data/aedes_aegypti_protein.fa"
+	aeal := "/home/luca/Dosuments/data/aedes_albopictus_protein.fa"
+	dataAEAE := parseFiles(aeae)
+	dataAEAL := parseFiles(aeal)
+	compareFiles(dataAEAE, dataAEAL)
+}
