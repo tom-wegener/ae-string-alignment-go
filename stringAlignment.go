7c3c97d8373e6396da5815487d7928e30e7d8dd3
Tom Wegener <wegener.tom@outlook.com>
2019-01-10 12:12:26 +0100

    adding gitlab-ci functionality (ship data with the program-files)

--- stringAlignment.go
+++ stringAlignment.go
@@ -1,8 +1,8 @@
 package main
 
 func main() {
-	aeae := "/home/luca/Documents/data/aedes_aegypti_protein.fa"
-	aeal := "/home/luca/Documents/data/aedes_albopictus_protein.fa"
+	aeae := "data/aedes_aegypti_protein.fa"
+	aeal := "data/aedes_albopictus_protein.fa"
 	dataAEAE := parseFiles(aeae)
 	dataAEAL := parseFiles(aeal)
 	compareFiles(dataAEAE, dataAEAL)
