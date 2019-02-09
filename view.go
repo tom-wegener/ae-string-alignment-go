36abdc481be658481224d44b23bd4d086410480c
Tom Wegener <wegener.tom@outlook.com>
2019-02-09 18:29:26 +0100

    adding the printing-ability

--- view.go
+++ view.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"sort"
 
 	"gonum.org/v1/plot"
@@ -11,49 +13,46 @@ import (
 
 func plotIt(runTimesNoS, runTimesRaS, runTimesNoL, runTimesRaL []times) {
 
+	fmt.Println("starting to plot")
 	p, err := plot.New()
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 
 	p.Title.Text = "benötigte Zeit"
 	p.X.Label.Text = "Datenlänge"
 	p.Y.Label.Text = "benötigte Zeit in Sekunden"
 
 	err = plotutil.AddLinePoints(p,
-		"Fasta Short", plotTimes(runTimesNoS),
-		"Zufall Short", plotTimes(runTimesRaS),
-		"Fasta Long", plotTimes(runTimesNoL),
-		"Zufall Long", plotTimes(runTimesRaL),
+		"Fasta Short", plotTimes(runTimesNoS, "NormalShort"),
+		"Zufall Short", plotTimes(runTimesRaS, "RandomShort"),
+		"Fasta Long", plotTimes(runTimesNoL, "NormalLangsam"),
+		"Zufall Long", plotTimes(runTimesRaL, "RandomLangsam"),
 	)
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 
 	// Save the plot to a PNG file.
-	if err := p.Save(10*vg.Inch, 10*vg.Inch, "points.png"); err != nil {
-		panic(err)
-	}
+	err = p.Save(10*vg.Inch, 10*vg.Inch, "points.png")
+	check(err)
 }
 
-func plotTimes(runTimes []times) plotter.XYs {
-	//sortedTimes := sortTimes(runTimes)
+func plotTimes(runTimes []times, idStr string) plotter.XYs {
 	sort.Sort(runTimesArr(runTimes))
 	sortedTimes := runTimes
-	//sortedTimes := runTimes
 	n := len(sortedTimes)
 	pts := make(plotter.XYs, n)
 	for i, row := range sortedTimes {
 		pts[i].X = float64(row.strLen)
 		pts[i].Y = float64(row.runTime)
 	}
+	printTimes(pts, idStr)
 	return pts
 }
 
-/*func sortTimes(runTimes []times) (sortedTimes []times) {
-	sortedTimes = runTimes
-	sort.Slice(sortedTimes, func(i, j int) bool {
-		return sortedTimes[i].len < sortedTimes[j].len
-	})
-	return sortedTimes
-}*/
+func printTimes(pts plotter.XYs, idStr string) {
+	fmt.Println("algorithm is done")
+	f, err := os.Create(idStr)
+	check(err)
+	for i := range pts {
+		fmt.Fprintf(f, "%v , %v \n", pts[i].X, pts[i].Y)
+	}
+	f.Close()
+}
