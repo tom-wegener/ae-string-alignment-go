0a37ab778e9c812bc5f0fbf23cfc003cd1753d71
Tom Wegener <wegener.tom@outlook.com>
2019-02-09 18:32:17 +0100

    making the sorting dependend on the string-length

--- objects.go
+++ objects.go
@@ -19,4 +19,4 @@ type runTimesArr []times
 
 func (c runTimesArr) Len() int           { return len(c) }
 func (c runTimesArr) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
-func (c runTimesArr) Less(i, j int) bool { return c[i].runTime > c[j].runTime }
+func (c runTimesArr) Less(i, j int) bool { return c[i].strLen > c[j].strLen }
