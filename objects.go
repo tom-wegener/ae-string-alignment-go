34425b634221bc46de40346297e1cb31243e487b
Tom Wegener <wegener.tom@outlook.com>
2019-02-23 14:54:39 +0100

    adding the score-object

--- objects.go
+++ objects.go
@@ -16,3 +16,10 @@ type runTimesArr []times
 func (c runTimesArr) Len() int           { return len(c) }
 func (c runTimesArr) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
 func (c runTimesArr) Less(i, j int) bool { return c[i].strLen > c[j].strLen }
+
+type scores struct {
+	key   string
+	score int
+}
+
+type scoresArr []scores
